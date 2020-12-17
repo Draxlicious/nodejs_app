d7b9e88f728107fbc481b862fde9c71648311a63
Lukas Mark Jakobsen <43439073+Draxlicious@users.noreply.github.com>
2020-12-17 08:02:36 +0100

    process.env.port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 var path = require('path');
-const port = 3000
+const port = process.env.PORT || 3000
 
 // peg på directory views skal læses
 app.set('views', path.join(__dirname, 'server/views'));
